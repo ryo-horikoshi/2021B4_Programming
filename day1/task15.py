9ab95809a1043d781cfc88190b9d13469bb909b6
ryo-horikoshi <horikoshi.ryo@gmail.com>
2021-04-12 15:40:19 +0900

    task15

---
+++ day1/task15.py
@@ -0,0 +1,25 @@
+import task13
+import task14
+import numpy as np
+
+def tfidf(terms,docs):
+  ans = np.zeros((len(terms),len(docs)))
+  idf = []
+  tf = []
+  for i in range(len(terms)):
+    idf.append(task14.idf(terms[i],docs))
+
+  for i in range(len(terms)):
+    tfs = []
+    for j in range(len(docs)):
+      tfs.append(task13.tf(terms[j],docs[i]))
+    tf.append(tfs)
+  np_idf = np.array(idf)
+  np_tf = np.array(tf)
+  return np_tf*np_idf
+
+
+
+docs = [["リンゴ", "リンゴ"], ["リンゴ", "レモン"], ["レモン", "ミカン"]]
+terms = ["リンゴ", "レモン", "ミカン"]
+print(tfidf(terms,docs))
