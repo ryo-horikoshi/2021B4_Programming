100146f21bb20899747bde4bad40f2f678fe53fa
ryo-horikoshi <horikoshi.ryo@gmail.com>
2021-04-21 13:45:07 +0900

    task2

--- day2/task2.py
+++ day2/task2.py
@@ -4,7 +4,6 @@ and_gate = task1.Perceptron(1,1,2)
 nand_gate = task1.Perceptron(-1,-1.1,-2)
 or_gate = task1.Perceptron(1,1,0.5)
 
-
 x1_list = [1, 1, 0, 0]
 x2_list = [1, 0, 1, 0]
 for i in range(len(x1_list)):
