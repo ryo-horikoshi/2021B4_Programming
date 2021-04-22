e81829c9364025dd0e70d0a0d25bf34f63401a9a
ryo-horikoshi <horikoshi.ryo@gmail.com>
2021-04-23 01:25:30 +0900

    加えた

--- day2/task41.py
+++ day2/task41.py
@@ -1,19 +1,21 @@
 import task33
 import task34
 import task35
-import random
+import numpy as np
 
 class NeuralNetwork:
   def __init__(self,bias,standard＿deviation,in_size,hide_size,out_size):
     self.params = {}
-    self.params['W1'] = standard＿deviation *  np.random.random(in_size,hide_size)
+    self.grads = {}
+    self.params['W1'] = standard＿deviation *  np.random.randn(in_size,hide_size)
     self.params['b1'] = np.zeros(hide_size)
-    self.params['W2'] = standard＿deviation *  np.random.random(in_size,hide_size)
+    self.params['W2'] = standard＿deviation *  np.random.randn(in_size,hide_size)
     self.params['b2'] = np.zeros(hide_size)
     self.grads['W2'] = None
     self.grads['b2'] = None
     self.grads['W1'] = None
     self.grads['b1'] = None
+    self.acc = 0
     self.Sigmoid = task33.Sigmoid()
     self.Affine = task34.Affine()
     self.SoftmaxCrossEntropy = task35.SoftmaxCrossEntropy()
@@ -29,6 +31,11 @@ class NeuralNetwork:
 
   def loss(self, x, t):
     y = forward(x)
+    y_class = np.argmax(task35.softmax(y))
+    y_class = y_class.astype("int8")
+    ans = np.identity(10, dtype = "int8")[y_class]
+    if(np.allclose(ans, t)):
+      self.acc += 1
     return SoftmaxCrossEntropy(y,t)
 
   def backprop(self, x, t):
@@ -36,12 +43,12 @@ class NeuralNetwork:
     dx1 = SoftmaxCrossEntropy.backprop()
     dx2 = Sigmoid.backprop(dx1)
     dx3 = Affine.backprop(dx2)[0]
-    self.grads['W2'] = None = Affine.backprop(dx2)[1]
-    self.grads['b2'] = None = Affine.backprop(dx2)[2]
+    self.grads['W2']  = Affine.backprop(dx2)[1]
+    self.grads['b2']  = Affine.backprop(dx2)[2]
     dx4 = Sigmoid.backprop(dx3)
     dx5 = Affine.backprop(dx4)[0]
-    self.grads['W1'] = None = Affine.backprop(dx4)[1]
-    self.grads['b1'] = None = Affine.backprop(dx4)[2]
+    self.grads['W1']  = Affine.backprop(dx4)[1]
+    self.grads['b1']  = Affine.backprop(dx4)[2]
 
   def sgd(self, x, t, learning_rate):
     backprop(x,t)
